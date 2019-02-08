e84a09e0074cd413863999537f258a37a1bd8dda
suhdonghwi <hwidongsuh@gmail.com>
2019-02-09 01:02:37 +0900

    publicPath 설정 값 오류 수정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     runtimeCompiler: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/Dimigames/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/Diminigames/' : '/'
 };
